9d43e8450533c5c01b4bba91764295c17dc610bd
Kindeep <kindeepsingh@gmail.com>
2019-03-05 21:37:36 -0500

    second commit

--- src/Engine/Choice.java
+++ src/Engine/Choice.java
@@ -8,7 +8,21 @@ public interface Choice {
 
     Double getStaminaCost();
 
+    /**
+     * Returns a list with the weights for choice/traitList
+     *
+     * @return Integer[] weights
+     */
     Integer[] getTraitWeights();
+
+    /**
+     * Uses trait weights and information from the stamina 
+     *
+     * @return if the choice can be made
+     */
+    boolean canMakeChoice(Player player);
+
+
 
 
 
