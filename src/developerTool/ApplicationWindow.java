53ecad23ecfa3d317fa9b76e1cbee7bd59229c7b
peakyDicers <ian.chui@hotmail.com>
2019-03-15 17:19:44 -0400

    added GameModel class, read from GUI works.

--- src/developerTool/ApplicationWindow.java
+++ src/developerTool/ApplicationWindow.java
@@ -12,10 +12,15 @@ import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
 
 import developerTool.Panels.*;
+import developerTool.XMLBuilder.GameModel;
+import developerTool.XMLBuilder.XMLBuilder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ApplicationWindow {
 
 	private JFrame frmStarsheepDeveloperTool;
+	private GameModel gameModel;
 
 	/**
 	 * Launch the application.
@@ -44,45 +49,54 @@ public class ApplicationWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		gameModel = new GameModel();
+
 		frmStarsheepDeveloperTool = new JFrame();
 		frmStarsheepDeveloperTool.setTitle("Starsheep Developer Tool");
 		frmStarsheepDeveloperTool.setBounds(100, 100, 700, 450);
 		frmStarsheepDeveloperTool.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmStarsheepDeveloperTool.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frmStarsheepDeveloperTool.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
-		GeneralPanel generalPanel = new GeneralPanel();
+
+		GeneralPanel generalPanel = new GeneralPanel(gameModel);
 		tabbedPane.addTab("General", null, generalPanel, null);
-		
+
 		JPanel Missions = new JPanel();
 		tabbedPane.addTab("Missions", null, Missions, null);
 		Missions.setLayout(new BorderLayout(0, 0));
-		
+
 		MissionsPanel missionsPanel = new MissionsPanel();
 		Missions.add(missionsPanel);
-		
+
 		JPanel Traits = new JPanel();
 		tabbedPane.addTab("Traits", null, Traits, null);
-		
+
 		JToolBar toolBar = new JToolBar();
 		frmStarsheepDeveloperTool.getContentPane().add(toolBar, BorderLayout.NORTH);
-		
+
 		JButton newGameBtn = new JButton("New Game");
 		toolBar.add(newGameBtn);
-		
+
 		JButton openGameBtn = new JButton("Open Game");
 		toolBar.add(openGameBtn);
-		
+
 		JButton exportBtn = new JButton("Export Game");
 		toolBar.add(exportBtn);
-		
+
 		JPanel panel = new JPanel();
 		toolBar.add(panel);
 		panel.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-		
+
 		JButton saveBtn = new JButton("Save");
+		saveBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				XMLBuilder.buildGameModelData(gameModel);
+			}
+		});
 		saveBtn.setHorizontalAlignment(SwingConstants.RIGHT);
 		panel.add(saveBtn);
 	}
